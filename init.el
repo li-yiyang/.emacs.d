863b6235f77579721cef85ea8a003c1f1e5a4414
凉凉 <thebigbigwordl@qq.com>
2023-11-30 21:14:51 +0800

    New: support with LSP via lsp-mode.
    * lsp-java for java code (to fix old physics code)
      (i shall remove it in the future...)

--- init.el
+++ init.el
@@ -351,6 +351,30 @@ for example:
   :hook ((irony-mode . irony-eldoc))
   :after (irony))
 
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((lsp-mode . lsp-enable-which-key-integration))
+  :commands (lsp))
+
+(use-package which-key
+  :config
+  (which-key-mode))
+
+(use-package lsp-java
+  :hook ((java-mode . lsp))
+  :after (lsp)
+  :config
+  (add-hook 'java-mode-hook
+            (lambda ()
+              (setq c-default-style "java"
+                    c-basic-offset 2
+                    tab-width 2
+                    indent-tabs-mode nil)
+              (c-set-offset 'arglist-intro '+)
+              (c-set-offset 'arglist-close '0)
+              (c-set-offset 'case-label '+))))
+
 (use-package org
   :config
   ;; org-mode and babel
