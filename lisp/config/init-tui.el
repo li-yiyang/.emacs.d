e949ca8fcc8e5e55c18216b1aee2dccff1ad26e8
凉凉 <thebigbigwordl@qq.com>
2024-08-12 15:41:14 +0800

    Add TUI support.

--- lisp/config/init-tui.el
+++ lisp/config/init-tui.el
@@ -1,5 +1,16 @@
 ;;; init-tui.el --- Init for TUI.
 
+;; use tao-yang as theme, prevent blink-search error
+;; see https://github.com/manateelazycat/blink-search/issues/16
+
+(require 'tao-theme)
+(require 'tao-yang-theme)
+(load-theme 'tao-yang t)
+
+;; who don't like mouse manipulation?
+
+(xterm-mouse-mode t)
+
 (provide 'init-tui)
 
 ;;; init-tui.el ends here
