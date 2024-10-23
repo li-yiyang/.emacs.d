e95e5015fcea8584d7df4932d5252e8e417ecfc8
凉凉 <56211811+li-yiyang@users.noreply.github.com>
2024-10-23 14:09:39 +0800

    update on yasnippet.

--- lisp/config/init-yas.el
+++ lisp/config/init-yas.el
@@ -4,6 +4,33 @@
 
 (require 'yasnippet)
 
+(defgroup ryo.yas ()
+  "Ryo's configures for yasnippet. "
+  :prefix "ryo.yas:")
+
+;; snippet functions predefined
+
+(defcustom ryo.yas:user-name (user-full-name)
+  "The username to be used when expanding yasnippet
+about the author and maintainer. "
+  :group 'ryo.yas
+  :type  'string)
+
+(defcustom ryo.yas:default-project-url "https://li-yiyang.github.io"
+  "The default URL when expanding yasnippet about project URL. "
+  :group 'ryo.yas
+  :type  'string)
+
+(defun ryo.yas:user-full-name ()
+  "Return the \\=`ryo.yas:user-name\\='. "
+  ryo.yas:user-name)
+
+(defun ryo.yas:project-url ()
+  "Return the project URL for snippet insert.
+By default it is \\=`ryo.yas:default-project-url\\='. "
+  ;; TODO: add more rules for the project url expanding
+  ryo.yas:default-project-url)
+
 ;; private snippet
 
 (add-to-list 'yas-snippet-dirs
