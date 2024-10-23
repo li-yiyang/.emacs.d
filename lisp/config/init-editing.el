7732fbe8c6b84822161a9642635124841db31e47
凉凉 <56211811+li-yiyang@users.noreply.github.com>
2024-10-23 13:56:39 +0800

    some small updates.

--- lisp/config/init-editing.el
+++ lisp/config/init-editing.el
@@ -26,9 +26,37 @@
 (require 'separedit)
 
 (define-key prog-mode-map (kbd "C-c '") #'separedit)
-(setq separedit-default-mode 'fundamental-mode)
+(setq separedit-default-mode                'text-mode)
+(setq separedit-code-block-default-mode     'text-mode)
 (setq separedit-preserve-string-indentation nil)
-(setq separedit-continue-fill-column nil)
+(setq separedit-continue-fill-column        nil)
+
+;; Manual save
+
+(defvar ryo.edit:before-manual-save-hook nil
+  "Hook called before function `ryo.edit:manual-save'. ")
+
+(defun ryo.edit:manual-save ()
+  "Manual Save.
+
+This shall be bind with `C-x C-s' to differ with
+auto save (see `init-autosave.lisp' for config)"
+  (interactive)
+  (run-hooks 'ryo.edit:before-manual-save-hook)
+  (save-buffer))
+
+(global-set-key (kbd "C-x C-s") #'ryo.edit:manual-save)
+
+(defun ryo.edit:update-last-upated ()
+  "Update Last-Updated infomation. "
+  (when (buffer-file-name)
+    (save-excursion
+      (goto-char (point-min))
+      (when (re-search-forward "Last-Updated: [[:digit:]]\\{4\\}-[[:digit:]]\\{1,2\\}-[[:digit:]]\\{1,2\\} [[:digit:]]\\{1,2\\}:[[:digit:]]\\{1,2\\}" nil t)
+        (replace-match
+         (concat "Last-Updated: " (format-time-string "%Y-%m-%d %H:%M")))))))
+
+(add-hook 'ryo.edit:before-manual-save-hook #'ryo.edit:update-last-upated)
 
 ;; Large file editing support:
 ;; from https://emacs-china.org/t/topic/25811/9
