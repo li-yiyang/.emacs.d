6b1d6ad54671a7234674677911d883afd2e25aee
凉凉 <56211811+li-yiyang@users.noreply.github.com>
2025-03-18 04:37:58 +0800

    Better eshell interface.

--- lisp/config/init-eshell.el
+++ lisp/config/init-eshell.el
@@ -1,8 +1,16 @@
-;;; init-eshell.el --- Set up eshell
+;;; init-eshell.el --- Set up eshell -*- lexical-binding: t -*-
 
 (require 'cl-lib)
 (require 'eshell)
 (require 'emojishell)
+
+;; by default using ssh
+
+(setq tramp-default-method "ssh")
+
+;; kill visual buffers
+
+(setq eshell-destroy-buffer-when-process-dies t)
 
 ;; history files saved to privates
 
@@ -30,8 +38,8 @@
   (let ((ebuff '()))
     (cl-dolist (buff (buffer-list))
       (with-current-buffer buff
-	(when (derived-mode-p 'eshell-mode)
-	  (push buff ebuff))))
+  (when (derived-mode-p 'eshell-mode)
+    (push buff ebuff))))
     ebuff))
 
 ;; default open new eshell buffer when calling `eshell'
@@ -42,14 +50,75 @@
   (if args (funcall fn args)
     ;; by default open new eshell on *different* dir
     (let* ((cwd default-directory)
-	   (buff (cl-find-if
-		  #'(lambda (buff)
-		      (string= cwd (buffer-local-value 'default-directory buff)))
-		  (ryo.eshell:eshell-buffers))))
+           (buff (cl-find-if
+                  #'(lambda (buff)
+                      (string= cwd (buffer-local-value 'default-directory buff)))
+                  (ryo.eshell:eshell-buffers))))
       (if buff
-	  (switch-to-buffer buff)
-	(funcall fn '(N))))))
+          (switch-to-buffer buff)
+        (funcall fn '(N))))))
 (advice-add 'eshell :around #'ryo.eshell:eshell-advice)
+
+;; eshls: list Eshell buffer
+(defun eshell/eshls (&rest args)
+  "List all eshell buffer names.
+Click to switch to eshell buffer. "
+  (cl-loop for buffer in (buffer-list)
+           if (eq (buffer-local-value 'major-mode buffer) 'eshell-mode)
+           do (eshell-printn
+               (propertize
+                (buffer-name buffer)
+                'category 'default-button
+                'button   (buffer-name buffer)
+                'action   #'(lambda (&rest args)
+                              (interactive)
+                              (let ((buffer (get-text-property (point) 'button)))
+                                (message buffer)
+                                (switch-to-buffer buffer)))
+                'help-echo   (concat "Switch to eshell buffer: `"
+                                     (buffer-name buffer) "'. ")
+                'follow-link t))))
+
+;; imgcat: show image in eshell
+;; see https://emacs-china.org/t/imgcat-eshell/3439
+(defun eshell/imgcat (&rest args)
+  "Display `images' in eshell. "
+  (if eshell-in-pipeline-p
+      (error "Elisp function does not support piped input. ")
+    (let (width height max-width max-height scale)
+      (eshell-eval-using-options
+       "imgcat" args
+       '((nil "width"      t width      "width of image(s)")
+         (nil "height"     t height     "height of image(s)")
+         (nil "max-width"  t max-width  "max width of image(s) [default 400]")
+         (nil "max-height" t max-height "max height of image(s)")
+         (?h  "help"   nil nil "show this usage screen")
+         :show-usage
+         :usage "[OPTION] IMAGE...
+Show IMAGE(s) file in eshell. ")
+       ;; TODO: better arg parse
+       (let ((property `(,@(when max-width
+                             (list :max-width (string-to-number max-width)))
+                         ,@(when max-height
+                             (list :max-height (string-to-number max-height)))
+                         ,@(when (and width
+                                      (or (not max-width)
+                                          (< (string-to-number width)
+                                             (string-to-number max-width))))
+                             (list :width (string-to-number width)))
+                         ,@(when (and height (or (not max-height)
+                                                 (< (string-to-number height)
+                                                    (string-to-number max-height))))
+                             (list :height (string-to-number height))))))
+         (when (endp property)
+           (setf property '(:max-width 400)))
+         (if (endp args)
+             (eshell-show-usage "image" nil)
+           (dolist (img (eshell-flatten-list args))
+             (eshell-printn
+              (propertize
+               img
+               'display (apply #'create-image (expand-file-name img) nil nil property))))))))))
 
 (provide 'init-eshell)
 
